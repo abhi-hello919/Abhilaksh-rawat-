03a4ad8101eda8cab7a8e309a72438ee704e8cd1
Abhilaksh Rawat <abhilakshrawat1997@gmail.com>
2025-10-28 09:11:09 +0530

    Create Q3.c
    address

---
+++ Q3.c
@@ -0,0 +1,11 @@
+//Write a C Program to print the address in multiple lines (new line)
+#include <stdio.h>
+
+int main() {
+    printf("Your Name: Abhilaksh Rawat \n");
+    printf("Street Address :123 Main Street\n");
+    printf("City, State, ZIP Code : Dehradun, Uttarakhand, 248001\n");
+    printf("Country : India\n");
+
+    return 0;
+}
