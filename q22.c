ad99a73ea3f7b9f4bf56f96fcdea04ff7d64e58f
Abhilaksh Rawat <abhilakshrawat1997@gmail.com>
2025-11-04 11:51:40 +0530

    program to reverse the elements

---
+++ q22.c
@@ -0,0 +1,41 @@
+//Write a C program to reverse the elements of an array using a function.
+#include <stdio.h>
+
+// Function to reverse the array
+void reverseArray(int arr[], int n) {
+    int start = 0, end = n - 1, temp;
+
+    while (start < end) {
+        // Swap elements
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+    
+}
